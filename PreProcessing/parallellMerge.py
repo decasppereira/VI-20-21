af717ee4dbbba20e4f31e3de1512bc0103d90552
decasppereira <43865518+decasppereira@users.noreply.github.com>
2021-10-28 14:51:32 +0100

    Create parallellMerge.py

---
+++ PreProcessing/parallellMerge.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+country_list = ["Austria","Belgium","Bulgaria","Cyprus","Denmark","Czechia","Croatia","Estonia","Finland","France","Germany","Greece","Hungary","Ireland","Italy","Iceland","Lithuania","Luxembourg","Malta","Netherlands","Norway","Poland","Portugal","Romania","Sweden","Slovenia","Slovakia","Spain","Turkey"]
+
+airQ = pd.read_csv("../data/air_quality_CO.csv", delimiter=",", encoding="latin1")
+fires = pd.read_csv("../data/fires_hectars.csv", delimiter=",", encoding="latin1")
+emissions = pd.read_csv("../data/emissions_totals.csv", delimiter=",", encoding="latin1")
+temp = pd.read_csv("../data/annual_avg_temp.csv", delimiter=",", encoding="latin1")
+
+airQ = airQ.groupby(["Country"]).mean()
+airQ.drop(["Year"],axis=1,inplace=True)
+
+fires = fires.groupby(["Country"]).mean()
+fires.drop(["Year"],axis=1,inplace=True)
+
+print(fires)
+
+merged = pd.DataFrame()
