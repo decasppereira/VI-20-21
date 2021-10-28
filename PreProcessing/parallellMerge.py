334cde7a17cccc8d07cb22cdfbd79b1188b76b97
decasppereira <43865518+decasppereira@users.noreply.github.com>
2021-10-28 15:01:04 +0100

    Updated datasets structure

--- PreProcessing/parallellMerge.py
+++ PreProcessing/parallellMerge.py
@@ -5,7 +5,7 @@ country_list = ["Austria","Belgium","Bulgaria","Cyprus","Denmark","Czechia","Cro
 
 airQ = pd.read_csv("../data/air_quality_CO.csv", delimiter=",", encoding="latin1")
 fires = pd.read_csv("../data/fires_hectars.csv", delimiter=",", encoding="latin1")
-emissions = pd.read_csv("../data/emissions_totals.csv", delimiter=",", encoding="latin1")
+emissions = pd.read_csv("../data/emissions_totals_renewed.csv", delimiter=",", encoding="latin1")
 temp = pd.read_csv("../data/annual_avg_temp.csv", delimiter=",", encoding="latin1")
 
 airQ = airQ.groupby(["Country"]).mean()
@@ -14,6 +14,9 @@ airQ.drop(["Year"],axis=1,inplace=True)
 fires = fires.groupby(["Country"]).mean()
 fires.drop(["Year"],axis=1,inplace=True)
 
-print(fires)
+emissions = emissions.groupby(["Country"]).mean()
+print(emissions)
+
+
 
 merged = pd.DataFrame()
